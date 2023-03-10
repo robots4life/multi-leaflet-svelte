b8757f6163e5237b5d0ace4bded0720f8e077e47
robots4life <34193243+robots4life@users.noreply.github.com>
2023-03-10 13:42:10 +0100

    add concerts data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+	import LeafletMap from '$components/LeafletMap.svelte';
+
+	const concerts = [
+		{ latitude: 47.37515142664481, longitude: 8.534933254122734, zoom: 17 },
+		{ latitude: 47.3847973, longitude: 8.5201642, zoom: 13 }
+	];
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each concerts as concert}
+	<LeafletMap latitude={concert.latitude} longitude={concert.longitude} zoom={concert.zoom} />
+{/each}
